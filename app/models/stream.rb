6ee76c7fb73355012032191175545fd801ba3f32
bhuvanamalini <bhuvanamalini333@gmail.com>
2019-12-18 10:23:38 +0530

    Update stream.rb

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -2,10 +2,10 @@ class Stream < ApplicationRecord
   attr_reader :query 
   def client 
       @client = Twitter::REST::Client.new(
-        :consumer_key => "SLW1Ge1p6vtZQGlsuNIQiYGDG",
-        :consumer_secret => "JiI7UJBcmuKxpGKUXDy23hzrJEuIsWhJ5Z99Je7Xwd5MECETvD",
-        :oauth_token => "1199676253319548928-ydCRxDBOOitnqTlQAFghXNpvaAJcq5",
-        :oauth_token_secret => "GVUUJxr02IhXgrB1bN7rrtuieZvC1qQA7cvxPIGz4j4u5"
+        :consumer_key => "SECRET KEY",
+        :consumer_secret => "CONSUMER SECRET",
+        :oauth_token => "OUTH KEY",
+        :oauth_token_secret => "OUTH SECRET"
         )
   end 
   def search(query) 
